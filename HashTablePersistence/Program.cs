16aa5e04733ab5a3fe97504cdf14b1654e8f72fe
ridethepig <protonghost@163.com>
2018-05-13 15:15:41 +0800

    first commit

--- HashTablePersistence/Program.cs
+++ HashTablePersistence/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
+using System.IO.Compression;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HashTablePersistence
 {
@@ -10,6 +13,72 @@ namespace HashTablePersistence
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    class PersistenceHelper
+    {
+        private Hashtable hashtable;
+        private bool compressFlag;
+        public PersistenceHelper(Hashtable table)
+        {
+            hashtable = table;
+            compressFlag = true;
+        }
+        
+        public void EnableCompress()
+        {
+            compressFlag = true;
+        }
+
+        public void DisableCompress()
+        {
+            compressFlag = false;
+        }
+        
+        protected static byte[] CompressBase(byte[] input)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                using (GZipStream gzipStream = new GZipStream(outStream, CompressionMode.Compress, true))
+                {
+                    gzipStream.Write(input, 0, input.Length);
+                    gzipStream.Close();
+                    return outStream.ToArray();
+                }
+            }
+        }
+
+        protected static byte[] DecompressBase(byte[] input)
+        {
+
+            using (MemoryStream inputStream = new MemoryStream(input))
+            {
+                using (MemoryStream outStream = new MemoryStream())
+                {
+                    using (GZipStream gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                    {
+                        gzipStream.CopyTo(outStream);
+                        gzipStream.Close();
+                        return outStream.ToArray();
+                    }
+                }
+
+            }
+        }
+
+        protected static string Compress(string inputString)
+        {
+            byte[] inputBytes = Encoding.Default.GetBytes(inputString);
+            byte[] outputBytes = CompressBase(inputBytes);
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        protected static string Decompress(string inputString)
+        {
+            byte[] inputBytes = Convert.FromBase64String(inputString);
+            byte[] outputBytes = DecompressBase(inputBytes);
+            return Encoding.Default.GetString(outputBytes);
         }
     }
 }
